a34b8c088a7da3134ea52cacb67a86a200947a7c
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2022-02-07 15:07:36 -0800

    Test functional component hooks

--- learn-hooks/src/App.js
+++ learn-hooks/src/App.js
@@ -1,11 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import styled  from 'styled-components';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('black');
 
 
-function App() {
+  const incrementCount = () => {
+    setCount(count + 1);
+  }
+  const decrementCount = () => {
+    setCount(count - 1);
+  }
+
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if (color === 'black') return setColor('red');
+      return setColor('black');
+    }
+    document.addEventListener('click', changeColorOnClick);
+
+    return () => {
+      document.removeEventListener('click', changeColorOnClick);
+    }
+  },[color]);
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div>
+      <div>{count}</div>
+      <button onClick={incrementCount} disabled={count >= 10}>Increment</button>
+      <button onClick={decrementCount} disabled={count <= 0}>Decrement</button>
+      <ClickDiv
+        color={color}
+        id="myDiv"
+      >
+        This div can change color. Click on me!
+      </ClickDiv>
+
     </div>
   );
 }
+
+
+const ClickDiv = styled.div`
+  color: white;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  background: ${props => props.color};
+`;
 
 export default App;
